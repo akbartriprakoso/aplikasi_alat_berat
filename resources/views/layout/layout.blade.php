a66fe684a43d6913a7873eab8ab7c25caa16a066
dzikyhuda <indo.jgj@gmail.com>
2021-04-02 19:19:57 +0700

    tambahan tempate

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -14,8 +14,8 @@
 	<title>Neon | Blue Skin</title>
 
 	<link rel="stylesheet" href="{{asset('assets/assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css')}}">
-	<link rel="stylesheet" href="{{asset('assets/assets/css/font-icons/entypo/css/entypo.css>
-	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic')}}">
+	<link rel="stylesheet" href="{{asset('assets/assets/css/font-icons/entypo/css/entypo.css')}}">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
 	<link rel="stylesheet" href="{{asset('assets/assets/css/bootstrap.css')}}">
 	<link rel="stylesheet" href="{{asset('assets/assets/css/neon-core.css')}}">
 	<link rel="stylesheet" href="{{asset('assets/assets/css/neon-theme.css')}}">
